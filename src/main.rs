b6a08554097e724e93ac3794c70199e8d1cada57
Niffirg-DcM <mcdonald.w.griffin@gmail.com>
2026-06-03 22:20:14 -0700

    sends correctly, does not unfold correctly

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use dotenvy::dotenv;
 
 //incoming payloads with deserialize
 #[derive(Deserialize, Debug)]
-struct in_candidates {
-    content: Vec<in_contents>,
+struct inCandidates {
+    content: Vec<inContents>,
 }
 
 #[derive(Deserialize, Debug)]
-struct in_contents {
-    part: Vec<in_parts>,
+struct inContents {
+    part: Vec<inParts>,
 }
 
 #[derive(Deserialize, Debug)]
-struct in_parts {
+struct inParts {
     data: Vec<String>,
 }
 
@@ -23,18 +23,18 @@ struct in_parts {
 
 //outgoing payloads with serialize
 #[derive(Serialize)]
-struct out_candidates {
-    content: Vec<out_contents>,
+struct outCandidates {
+    contents: Vec<outContents>,
 }
 
 #[derive(Serialize)]
-struct out_contents {
-    part: Vec<out_parts>,
+struct outContents {
+    parts: Vec<outParts>,
 }
 
 #[derive(Serialize)]
-struct out_parts {
-    data: Vec<String>,
+struct outParts {
+    text: String,
 }
 
 #[tokio::main]
@@ -43,32 +43,41 @@ async fn main() -> Result<(),Box<dyn std::error::Error>> {
 
     
     let key = match env::var("GEMINI_API_KEY") {
-        Ok(val) => println!("key recieved"),
-        Err(e) => eprintln!("Error reading gemini api key"),
+        Ok(val) => val,
+        Err(e) => String::from("Error"),
     };
+
+    if(key.eq("Error")) {
+        println!("no Key");
+    }
+
+
 
     println!("Hello, world!");
     println!("Exit anytime with 'exit'");
-    
+
+    println!("Please enter your query: ");
+    let mut data_to_send = String::new();
+    io::stdin().read_line(&mut data_to_send).expect("damn");
+
+
     //("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={}",apiKey.trim()))
     let client = reqwest::Client::builder()
         .timeout(Duration::from_secs(10))
         .build()?;
 
-    let get_url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={}",apiKey.trim());
-    
-    let response = client.get(get_url).send().await?;
+    let url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={}",key);
 
-    let post_url = "https://typicode.com";
-    let payload = out_candidates {
-        content: vec![ out_contents {
-            part: vec![ out_parts {
-                data: vec![String::from("fail")]}
+
+    let payload = outCandidates {
+        contents: vec![ outContents {
+            parts: vec![ outParts {
+                text: String::from("fail")}
                 ]
             } ]
     };
 
-    let post_response = client.post(post_url)
+    let post_response:inCandidates = client.post(url)
         .header("Content-Type", "application/json")
         .json(&payload)
         .send()
@@ -76,6 +85,9 @@ async fn main() -> Result<(),Box<dyn std::error::Error>> {
 
     if post_response.status().is_success() {
         println!("POST Success! Resource created.");
+        println!("{}",);
+    } else {
+        println!("Error Code: {}",post_response.status().to_string());
     }
 
     Ok(())
