c8d43a608baae2e24bdf588b4e8e6dd796f19f0f
Niffirg-DcM <mcdonald.w.griffin@gmail.com>
2026-06-05 12:49:13 -0700

    looped for repeated queries

--- src/main.rs
+++ src/main.rs
@@ -61,43 +61,44 @@ async fn main() -> Result<(),Box<dyn std::error::Error>> {
 
     println!("Hello, world!");
     println!("Exit anytime with 'exit'");
+    loop {
+        println!("Please enter your query: ");
+        let mut data_to_send = String::new();
+        io::stdin().read_line(&mut data_to_send).expect("damn");
+        if (data_to_send.trim()=="exit") {break;}
 
-    println!("Please enter your query: ");
-    let mut data_to_send = String::new();
-    io::stdin().read_line(&mut data_to_send).expect("damn");
+        //("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={}",apiKey.trim()))
+        let client = reqwest::Client::builder()
+            .timeout(Duration::from_secs(10))
+            .build()?;
 
-
-    //("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={}",apiKey.trim()))
-    let client = reqwest::Client::builder()
-        .timeout(Duration::from_secs(10))
-        .build()?;
-
-    let url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={}",key);
-
-
-    let payload = outCandidates {
-        contents: vec![ outContents {
-            parts: vec![ outParts {
-                text: data_to_send}
-                ]
-            } ]
-    };
-
-    let post_response:Response = client.post(url)
-        .header("Content-Type", "application/json")
-        .json(&payload)
-        .send()
-        .await?;
+        let url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={}",key);
 
 
+        let payload = outCandidates {
+            contents: vec![ outContents {
+                parts: vec![ outParts {
+                    text: data_to_send}
+                    ]
+                } ]
+        };
 
-    if post_response.status().is_success() {
-        println!("POST Success! Resource created.");
-        let parsed_response: GeminiResponse = post_response.json().await?;
-        println!("Response is: {}",parsed_response.candidates[0].content.parts[0].text);
-        
-    } else {
-        println!("Error Code: {}",post_response.status().to_string());
+        let post_response:Response = client.post(url)
+            .header("Content-Type", "application/json")
+            .json(&payload)
+            .send()
+            .await?;
+
+
+
+        if post_response.status().is_success() {
+            println!("POST Success! Resource created.");
+            let parsed_response: GeminiResponse = post_response.json().await?;
+            println!("Response is: {}",parsed_response.candidates[0].content.parts[0].text);
+            
+        } else {
+            println!("Error Code: {}",post_response.status().to_string());
+        }
     }
 
     Ok(())
